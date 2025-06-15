fe1a15208a74f93a28be05e56b60795befed07a7
BECCABOOTY <vintagefreak001@gmail.com>
2025-06-15 18:59:40 +0100

    form and app

--- week7fullstackApp/client/src/components/Form.jsx
+++ week7fullstackApp/client/src/components/Form.jsx
@@ -1,19 +1,56 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [username, setUsername] = useState("");
+  const [guess, setGuess] = useState("");
+  const [theory, setTheory] = useState("");
+
   function handleSubmit(event) {
     event.preventdefault();
+  }
+  function handleUsername(event) {
+    setUsername(event.target.value);
+  }
+  function handleGuess(event) {
+    setGuess(event.target.value);
+  }
+  function handleTheory(event) {
+    setTheory(event.target.value);
   }
   return (
     <>
       <h1>Form</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
-          <legend>Title of Form</legend>
-          <label htmlFor="">Username:</label>
-          <input type="text" />
-          <label htmlFor="">Whodunnit?</label>
-          <input type="text" />
-          <label htmlFor="">Theory:</label>
-          <input type="text" />
+          <legend>Submit your theories:</legend>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleUsername}
+            required
+          />
+
+          <label htmlFor="guess">Whodunnit?</label>
+          <input
+            type="text"
+            name="guess"
+            value={guess}
+            onChange={handleGuess}
+            required
+          />
+
+          <label htmlFor="theory">Theory:</label>
+          <input
+            type="text"
+            name="theory"
+            value={theory}
+            onChange={handleTheory}
+            required
+          />
+
+          <button type="submit">Submit!</button>
         </fieldset>
       </form>
     </>
--- week7fullstackApp/server/server.js
+++ week7fullstackApp/server/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { db } from "./dbConnection.js";
@@ -15,4 +15,31 @@ app.listen(PORT, () => {
 
 app.get("/", (request, response) => {
   response.json({ message: "You're in the root route!" });
+});
+
+app.get("/butlertheories", async (request, response) => {
+  const data = await db.query('SELECT * FROM whodunnit WHERE guess = "Butler"');
+  response.json(data.rows);
+});
+
+app.get("/heiresstheories", async (request, response) => {
+  const data = await db.query(
+    'SELECT * FROM whodunnit WHERE guess = "Heiress"'
+  );
+  response.json(data.rows);
+});
+
+app.get("/reverendtheories", async (request, response) => {
+  const data = await db.query(
+    'SELECT * FROM whodunnit WHERE guess = "Reverend"'
+  );
+  response.json(data.rows);
+});
+
+app.post("/addtheory", (request, response) => {
+  const body = request.body;
+  const insert = db.query(
+    "INSERT INTO whodunnit (guess, theory, username_id) VALUES ($1, $2. $3)",
+    [body.guess, body.theory, body.username_id]
+  );
 });
